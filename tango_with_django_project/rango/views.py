a9d00984672fd29f8b77757ed3117828ef22f172
“Taeshin <2716660T@student.gla.ac.uk>
2023-02-10 14:25:02 +0000

    all

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    pass
     #return HttpResponse("Rango says hey there partner!")
     return HttpResponse('<a href="about">Rango says hey there partner!</a>')
+    
 
+def rango(request):
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     #return HttpResponse("Rango says here is the about page.")
-    return HttpResponse('<a href="/">Rango says here is the about page.</a>')
+    return render(request, 'rango/about.html')
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -17,12 +17,17 @@ from django.contrib import admin
 from django.urls import path, include
 from rango import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'rango'
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("about",views.about, name = "about"),
+    path("about/",views.about, name = "about"),
 
-    #path("rango/", include("rango.urls")),
+    path("rango/", views.rango, name = "Rango"),
 
     path("admin/", admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
